9b4790c09e90a4108dbf3733d4157f14e72eea9a
TNOK_AKUSHIROYO <qqnq7um9k@gmail.com>
2024-11-25 10:17:46 +0900

    added typeExample in 02_06

---
+++ 02_06/typeExample/src/TypeExample.java
@@ -0,0 +1,16 @@
+public class TypeExample {
+    public static void main(String[] args) {
+        double d;
+        d = 9.9;
+        char c;
+        c = 'あ';
+        byte b;
+        b = 81;
+        boolean bool;
+        bool = true;
+        System.out.println("dの値は" + d);
+        System.out.println("cの値は" + c);
+        System.out.println("bの値は" + b);
+        System.out.println("boolの値は" + bool);
+    }
+}
