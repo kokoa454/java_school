ce523e0311f9236cdcb8b07d818036e5fbce02ef
TNOK_AKUSHIROYO <qqnq7um9k@gmail.com>
2024-12-02 10:02:20 +0900

    added overloadExample in 04_10

---
+++ 04_10/overloadExample/src/OverloadExample.java
@@ -0,0 +1,24 @@
+public class OverloadExample {
+    public static void main(String[] args) {
+        methodA();
+        methodA(1);
+        methodA(0.1);
+        methodA("Hello");
+    }
+
+    public static void methodA(){
+        System.out.println("引数はありません");
+    }
+
+    public static void methodA(int i){
+        System.out.println("int型の値" + i + "を受け取りました");
+    }
+
+    public static void methodA(double d){
+        System.out.println("double型の値" + d + "を受け取りました");
+    }
+
+    public static void methodA(String s){
+        System.out.println("文字列" + s + "を受け取りました");
+    }
+}
