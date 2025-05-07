dca01d528249624e088c793aef127732a3c9d601
Inari Goro <qqnq7um9k@gmail.com>
2025-05-07 11:23:35 +0900

    added 0507_02

---
+++ second_grade/0507_02/src/ExceptionExample5.java
@@ -0,0 +1,19 @@
+class SimpleClass{
+    void doSomething(){
+        int array[] = new int[3];
+        array[10] = 99;
+        System.out.println("doSomethingメソッドを終了します");
+    }
+}
+
+public class ExceptionExample5 {
+    public static void main(String[] args) throws Exception {
+        SimpleClass obj = new SimpleClass();
+        try{
+            obj.doSomething();
+        } catch(ArrayIndexOutOfBoundsException e){
+            System.out.println("例外をキャッチしました");
+            e.printStackTrace();
+        }
+    }
+}
