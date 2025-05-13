c447bb54fd92804bec549d055215f0ff1cf15b0e
Inari Goro <qqnq7um9k@gmail.com>
2025-05-13 11:40:26 +0900

    updated exception1

--- Assignment/exception1/src/Exception1.java
+++ Assignment/exception1/src/Exception1.java
@@ -1,6 +1,13 @@
 public class Exception1 {
     public static void main(String[] args) throws Exception {
         String s = null;
-        System.out.println(s.length());
+
+        try{
+            System.out.println(s.length());
+        }catch(NullPointerException e){
+            System.out.println(e.getMessage());
+        } finally {
+            System.out.println("プログラムの実行を終了します。");
+        }
     }
 }
